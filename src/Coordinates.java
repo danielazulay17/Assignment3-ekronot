a5991c011d9a06ed56457cf018fc95a14814128f
danielazulay17 <azulayd@post.bgu.ac.il>
2023-06-21 19:39:55 +0300

    first commit

---
+++ src/Coordinates.java
@@ -0,0 +1,18 @@
+public class Coordinates {
+
+    private int x;
+    private int y;
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX (int x){
+        this.x = x;
+    }
+    public int getY() {
+        return y;
+    }
+}
+
+
---
+++ src/Unit.java
@@ -0,0 +1,11 @@
+public class Unit {
+
+    private String name;
+    private Health health;
+    private int attackPoints;
+    private int defensePoints;
+
+
+
+
+}
